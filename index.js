1e0328fdea4c7862bf929ed31998e8a400fe0fdb
dns2140 <davidsagala2140@gmail.com>
2025-04-01 23:11:51 +0700

    improve crud

--- index.js
+++ index.js
@@ -4,16 +4,37 @@ const globalState = {
 
 const userList = document.querySelector('.userList');
 const submitBtn = document.querySelector('#submit');
+
+const handleEdit = (id) => {
+  console.log(id);
+  updateUser(id);
+};
+
+const handleDelete = (id) => {
+  console.log(id);
+  deleteUser(id);
+};
+
 const updateDom = () => {
   userList.innerHTML = '';
   globalState.users.map((user) => {
-    userList.insertAdjacentHTML('beforeend', `<li>${user.name}</li>`);
+    userList.insertAdjacentHTML(
+      'beforeend',
+      `<li>${user.name}
+      <a 
+      class="editBtn" 
+      onclick="handleEdit(${user.id})"><button>edit</button></a>
+      <a class="deleteBtn" onclick="handleDelete(${user.id})"><button>delete</button></a>
+      </li>`
+    );
   });
 };
+
 const fetchData = async () => {
   try {
     const users = await axios('https://jsonplaceholder.typicode.com/users');
     globalState.users = [...users.data];
+    console.log(globalState.users);
     updateDom();
   } catch (error) {
     console.log('there is an error', error);
@@ -37,28 +58,64 @@ const postData = async () => {
   }
 };
 
-const updateUser = async () => {
+const updateUser = async (id) => {
   const updatedUser = {
     name: 'Davidddddd',
     username: 'dns2140000',
   };
   try {
+    //update server
     const response = await axios.put(
-      'https://jsonplaceholder.typicode.com/users/1',
+      `https://jsonplaceholder.typicode.com/users/${id}`,
       updatedUser
     );
     console.log('Updated user:', response.data);
+
+    //update global state
+
+    //findIndex
+    const userIndex = globalState.users.findIndex(
+      (user) => user.id === parseInt(id)
+    );
+
+    //validation
+    if (userIndex === -1) {
+      console.log('user not found');
+    }
+
+    //update global state
+    globalState.users = [
+      ...globalState.users.slice(0, userIndex),
+      {
+        ...globalState.users[userIndex],
+        ...updatedUser,
+      },
+      ...globalState.users.slice(userIndex + 1),
+    ];
+
+    updateDom();
+
+    console.log('found user:', userIndex);
   } catch (error) {
     console.log('there is an error', error);
+    throw error;
   }
 };
 
-const deleteUser = async () => {
+const deleteUser = async (id) => {
   try {
+    //update server
     const response = await axios.delete(
-      'https://jsonplaceholder.typicode.com/users/1'
+      `https://jsonplaceholder.typicode.com/users/${id}`
     );
     console.log('Deleted user:', response.data);
+
+    //update global state
+    globalState.users = globalState.users.filter(
+      (user) => user.id !== parseInt(id)
+    );
+
+    updateDom();
   } catch (error) {
     console.log('there is an error', error);
   }
@@ -68,4 +125,4 @@ submitBtn.addEventListener('click', () => {
   postData();
 });
 
-deleteUser();
+fetchData();
